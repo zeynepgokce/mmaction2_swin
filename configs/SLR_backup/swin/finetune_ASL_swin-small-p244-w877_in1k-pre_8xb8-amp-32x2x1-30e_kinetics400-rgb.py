c84559d8737d3ce7d6a1ec29333e226265c2e37b
zeynepgokce <zzeynepgokce@gmail.com>
2025-10-12 18:33:21 +0300

    swin_small config güncellendi.

---
+++ configs/SLR_backup/swin/finetune_ASL_swin-small-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb.py
@@ -0,0 +1,139 @@
+_base_ = [
+    '../../_base_/models/swin_tiny.py', '../../_base_/default_runtime.py'
+]
+
+model = dict(
+    backbone=dict(
+        arch='small',
+        drop_path_rate=0.2,
+        pretrained=  # noqa: E251
+        'https://download.openmmlab.com/mmaction/v1.0/recognition/swin/swin_small_patch4_window7_224.pth'  # noqa: E501
+    ),
+    cls_head=dict(num_classes=250)
+)
+# dataset settings
+dataset_type = 'RawframeDataset'
+dataset_root ="/arf/home/zgokce/code/videomae_v2/data"
+data_root = dataset_root +'/ASL-Text_64x64_640x480_PIL/train'
+data_root_val = dataset_root +'/ASL-Text_64x64_640x480_PIL/val'
+data_root_test = dataset_root +'/ASL-Text_64x64_640x480_PIL/test'
+ann_file_train = dataset_root +'/ASL-Text_64x64_640x480_PIL/train_mm2.txt'
+ann_file_val = dataset_root +'/ASL-Text_64x64_640x480_PIL/val_mm2.txt'
+ann_file_test = dataset_root +'/ASL-Text_64x64_640x480_PIL/test_mm2.txt'
+
+file_client_args = dict(io_backend='disk')
+train_pipeline = [
+    dict(type='SampleFrames', clip_len=16, frame_interval=1, num_clips=1),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandomResizedCrop'),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+val_pipeline = [
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+test_pipeline = [
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=4,
+        test_mode=True),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 224)),
+    dict(type='ThreeCrop', crop_size=224),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=dict(img=data_root),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=dict(img=data_root_val),
+        pipeline=val_pipeline,
+        test_mode=True))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=dict(img=data_root_test),
+        pipeline=test_pipeline,
+        test_mode=True))
+
+val_evaluator = dict(type='AccMetric')
+test_evaluator = val_evaluator
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=45, val_begin=1, val_interval=3)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=1e-4, betas=(0.9, 0.999), weight_decay=0.02),
+    constructor='SwinOptimWrapperConstructor',
+    paramwise_cfg=dict(
+        absolute_pos_embed=dict(decay_mult=0.),
+        relative_position_bias_table=dict(decay_mult=0.),
+        norm=dict(decay_mult=0.),
+        backbone=dict(lr_mult=0.1)))
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=0.1,
+        by_epoch=True,
+        begin=0,
+        end=2.5,
+        convert_to_iter_based=True),
+    dict(
+        type='CosineAnnealingLR',
+        T_max=30,
+        eta_min=0,
+        by_epoch=True,
+        begin=0,
+        end=30)
+]
+
+default_hooks = dict(
+    checkpoint=dict(interval=3, max_keep_ckpts=5), logger=dict(interval=100))
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (8 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=64)
+load_from = "/home/zeynep/Thesis/code/mmaction2/ckpt/swin-small-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb_20220930-e91ab986.pth"
