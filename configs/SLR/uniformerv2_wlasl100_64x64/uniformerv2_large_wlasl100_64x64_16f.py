bee474e282eb7769331b9d0f78a53f0eb04945d9
zeynepgokce <zzeynepgokce@gmail.com>
2026-02-18 01:01:19 +0300

    uniformer wlasl100 64x64 configler üretildi

---
+++ configs/SLR/uniformerv2_wlasl100_64x64/uniformerv2_large_wlasl100_64x64_16f.py
@@ -0,0 +1,164 @@
+_base_ = ['../../_base_/default_runtime.py']
+
+# model settings
+num_frames = 16
+model = dict(
+    type='Recognizer3D',
+    backbone=dict(
+        type='UniFormerV2',
+        pretrained='ViT-L/14',
+        input_resolution=224,
+        patch_size=14,
+        width=1024,
+        layers=24,
+        heads=16,
+        t_size=num_frames,
+        dw_reduction=1.5,
+        backbone_drop_path_rate=0.,
+        temporal_downsample=False,
+        no_lmhra=True,
+        double_lmhra=True,
+        return_list=[20, 21, 22, 23],
+        n_layers=4,
+        n_dim=1024,
+        n_head=16,
+        mlp_factor=4.,
+        drop_path_rate=0.,
+        mlp_dropout=[0.5, 0.5, 0.5, 0.5]),
+    cls_head=dict(
+        type='TimeSformerHead',
+        dropout_ratio=0.5,
+        num_classes=100,
+        in_channels=1024,
+        average_clips='prob'),
+    data_preprocessor=dict(
+        type='ActionDataPreprocessor',
+        mean=[114.75, 114.75, 114.75],
+        std=[57.375, 57.375, 57.375],
+        format_shape='NCTHW'))
+
+# dataset settings
+dataset_type = 'VideoDataset'
+dataset_root = '/arf/scratch/zgokce/data'
+data_root = dataset_root + '/WLASL/train'
+data_root_val = dataset_root + '/WLASL/val'
+data_root_test = dataset_root + '/WLASL/test'
+ann_file_train = dataset_root + '/WLASL/train_wlasl100_mm2.txt'
+ann_file_val = dataset_root + '/WLASL/val_wlasl100_mm2.txt'
+ann_file_test = dataset_root + '/WLASL/test_wlasl100_mm2.txt'
+
+file_client_args = dict(io_backend='disk')
+train_pipeline = [
+    dict(type='DecordInit', **file_client_args),
+    dict(type='UniformSample', clip_len=num_frames, num_clips=1),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(
+        type='PytorchVideoWrapper',
+        op='RandAugment',
+        magnitude=7,
+        num_layers=4),
+    dict(type='RandomResizedCrop'),
+    dict(type='Resize', scale=(64, 64), keep_ratio=False),
+    dict(type='Resize', scale=(256, 256), keep_ratio=False),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+
+val_pipeline = [
+    dict(type='DecordInit', **file_client_args),
+    dict(type='UniformSample', clip_len=num_frames, num_clips=1,
+         test_mode=True),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(64, 64), keep_ratio=False),
+    dict(type='Resize', scale=(256, 256), keep_ratio=False),
+    dict(type='Resize', scale=(-1, 224)),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+
+test_pipeline = [
+    dict(type='DecordInit', **file_client_args),
+    dict(type='UniformSample', clip_len=num_frames, num_clips=1,
+         test_mode=True),
+    dict(type='DecordDecode'),
+    dict(type='Resize', scale=(64, 64), keep_ratio=False),
+    dict(type='Resize', scale=(256, 256), keep_ratio=False),
+    dict(type='Resize', scale=(-1, 224)),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=dict(video=data_root),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=dict(video=data_root_val),
+        pipeline=val_pipeline,
+        test_mode=True))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=dict(video=data_root_test),
+        pipeline=test_pipeline,
+        test_mode=True))
+
+val_evaluator = dict(type='AccMetric')
+test_evaluator = dict(type='AccMetric')
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=40, val_begin=1, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+base_lr = 2e-6
+optim_wrapper = dict(
+    optimizer=dict(
+        type='AdamW', lr=base_lr, betas=(0.9, 0.999), weight_decay=0.05),
+    paramwise_cfg=dict(norm_decay_mult=0.0, bias_decay_mult=0.0),
+    clip_grad=dict(max_norm=20, norm_type=2))
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=0.5,
+        by_epoch=True,
+        begin=0,
+        end=1,
+        convert_to_iter_based=True),
+    dict(
+        type='CosineAnnealingLR',
+        T_max=4,
+        eta_min_ratio=0.5,
+        by_epoch=True,
+        begin=1,
+        end=5,
+        convert_to_iter_based=True)
+]
+
+default_hooks = dict(
+    checkpoint=dict(interval=3, max_keep_ckpts=5), logger=dict(interval=100))
+
+auto_scale_lr = dict(enable=False, base_batch_size=1)
+
+load_from = './ckpt/uniformerv2-large-p14-res224_clip-kinetics710-pre_u16_kinetics400-rgb_20221219-6dc86d05.pth'
